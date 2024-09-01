c71b991fbd97be38d9ce7461198d10fa8d70e801
FooKaDoO <mihkelhani@gmail.com>
2024-09-01 21:30:55 +0300

    Moved ExpirableEntry to the beginning. Refactored code

--- cache/simple_aged_cache.py
+++ cache/simple_aged_cache.py
@@ -1,15 +1,43 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 class SimpleAgedCache:
     """
-    Initializes a SimpleAgedCache priority queue.
+    ExpirableEntry inner class for entries that expire after a set amount of time.
+    """
+    class ExpirableEntry:
+        """
+        Initializes an ExpirableEntry object.
+
+        Parameters: value, expiration_time, clock
+        expiration_time is a datetime object of when the object should be cleaned up.
+        """
+        def __init__(self, 
+                     key, value, 
+                     expiration_time: datetime):
+            self.key = key
+            self.value = value
+            self.expiration_time: datetime = expiration_time
+            self.next = None
+
+        """
+        Returns True or False according to if the ExpriableEntry object is expired.
+        
+        Parameters: curr_time.
+        curr_time is current time as a datetime object.
+        """
+        def is_expired(self, curr_time: datetime):
+            return self.expiration_time <= curr_time
     
+    
+    """
+    Initializes a SimpleAgedCache priority queue.
+
     Parameters: clock=datetime.utcnow
     clock is a function which when called returns current time as a datetime object.
     """
-    def __init__(self, clock=datetime.utcnow):
+    def __init__(self, clock = lambda: datetime.now(timezone.utc)):
         self.clock = clock
-        self.head: SimpleAgedCache.ExpirableEntry = None
+        self.head: self.ExpirableEntry = None
         self.length: int = 0
 
 
@@ -24,11 +52,11 @@ class SimpleAgedCache:
             retention_in_millis: int):
         self.clean_expired()
         
-        new_elem: SimpleAgedCache.ExpirableEntry = self.create_entry(key, value, retention_in_millis)
-        curr_elem: SimpleAgedCache.ExpirableEntry = self.head
-        prev_elem: SimpleAgedCache.ExpirableEntry = None
+        new_elem: self.ExpirableEntry = self.ExpirableEntry(key, value, self.clock() + timedelta(milliseconds=retention_in_millis))
+        curr_elem: self.ExpirableEntry = self.head
+        prev_elem: self.ExpirableEntry = None
         
-        while curr_elem != None and new_elem.time_to_live() > curr_elem.time_to_live():
+        while curr_elem != None and new_elem.expiration_time > curr_elem.expiration_time:
             prev_elem, curr_elem = curr_elem, curr_elem.next
         
         new_elem.next = curr_elem
@@ -56,7 +84,7 @@ class SimpleAgedCache:
     Removes expired ExpirableEntry objects from SimpleAgedCache priority queue.
     """
     def clean_expired(self):
-        while self.head != None and self.head.is_expired():
+        while self.head != None and self.head.is_expired(self.clock()):
             self.head = self.head.next
             self.length -= 1
 
@@ -74,44 +102,4 @@ class SimpleAgedCache:
             if elem.key == key:
                 return elem.value
             elem = elem.next
-
         return None
-
-    """
-    Factory method for creating new ExpirableEntry objects.
-
-    Parameters: key, value, retention_in_millis.
-    retention_in_millis is the time the object is retained before cleaned up.
-    """
-    def create_entry(self, 
-                     key, value, 
-                     retention_in_millis):
-        return SimpleAgedCache.ExpirableEntry(key, value, 
-                                              self.clock() + timedelta(milliseconds=retention_in_millis), 
-                                              self.clock)
-    
-    """
-    ExpirableEntry inner class for entries that expire after a set amount of time.
-    """
-    class ExpirableEntry:
-        """
-        Initializes an ExpirableEntry object.
-
-        Parameters: value, expiration_time, clock
-        expiration_time is the time in milliseconds when the object should be cleaned up.
-        clock is a function which when called returns current time as a datetime object.
-        """
-        def __init__(self, 
-                     key, value, 
-                     expiration_time, 
-                     clock):
-            self.key = key
-            self.value = value
-            self.time_to_live = lambda: (expiration_time - clock())
-            self.next: SimpleAgedCache.ExpirableEntry = None
-
-        """
-        Returns True or False according to if the ExpriableEntry object is expired.
-        """
-        def is_expired(self):
-            return self.time_to_live() <= timedelta(0)
